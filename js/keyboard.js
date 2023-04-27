b5380d1dde3c4adbd50dd105023ab4f099ce6ff3
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-04-27 18:04:18 +0300

    feat: add capslock handling

--- js/keyboard.js
+++ js/keyboard.js
@@ -21,7 +21,7 @@ export default class Keyboard {
       }
     });
     document.addEventListener('keyup', (e) => {
-      document.querySelector(`.${e.code}`).classList.remove('active');
+      if (e.code !== 'CapsLock') document.querySelector(`.${e.code}`).classList.remove('active');
     });
   };
 
@@ -40,13 +40,10 @@ export default class Keyboard {
   createKey = (keyName) => {
     const key = createNode('div', `key ${keyName}`, KEYS[keyName][this.lang][this.layout]);
     key.id = keyName;
-    if (!this.fnKeys.includes(key.id)) {
-      key.addEventListener('mousedown', this.handleInputKeys);
-    } else {
-      key.addEventListener('mousedown', this.handleFnKeys);
-    }
+    if (this.fnKeys.includes(key.id)) key.addEventListener('mousedown', this.handleFnKeys);
+    else key.addEventListener('mousedown', this.handleInputKeys);
     key.addEventListener('mouseup', (e) => {
-      document.querySelector(`.${e.target.id}`).classList.remove('active');
+      if (e.target.id !== 'CapsLock') document.querySelector(`.${e.target.id}`).classList.remove('active');
     });
     return key;
   };
@@ -63,38 +60,45 @@ export default class Keyboard {
     e.preventDefault();
     this.output.focus();
     const keyName = e.code || e.target.id;
-    document.querySelector(`.${keyName}`).classList.add('active');
-    let cursorPoint = this.output.selectionStart;
-    const left = this.output.value.slice(0, cursorPoint);
-    const right = this.output.value.slice(cursorPoint);
+    if (keyName !== 'CapsLock') document.querySelector(`.${keyName}`).classList.add('active');
+    let cursorPos = this.output.selectionStart;
+    const left = this.output.value.slice(0, cursorPos);
+    const right = this.output.value.slice(cursorPos);
     switch (keyName) {
       case 'Backspace':
         this.output.value = `${left.slice(0, -1)}${right}`;
-        cursorPoint -= 1;
+        cursorPos -= 1;
         break;
       case 'Space':
         this.output.value = `${left} ${right}`;
-        cursorPoint += 1;
+        cursorPos += 1;
         break;
       case 'Delete':
         this.output.value = `${left}${right.slice(1)}`;
         break;
       case 'Enter':
         this.output.value = `${left}\n${right}`;
-        cursorPoint += 1;
+        cursorPos += 1;
         break;
       case 'CapsLock':
+        document.querySelector(`.${keyName}`).classList.toggle('active');
         this.layout = this.layout === 'caps' ? 'lowerCase' : 'caps';
-        this.keyboard.remove();
-        this.renderKeyboard();
+        this.switchLayout();
         break;
       case 'Tab':
         this.output.value = `${left}\t${right}`;
-        cursorPoint += 1;
+        cursorPos += 1;
         break;
       default:
         break;
     }
-    this.output.setSelectionRange(cursorPoint, cursorPoint);
+    this.output.setSelectionRange(cursorPos, cursorPos);
+  };
+
+  switchLayout = () => {
+    document.querySelectorAll('.key').forEach((element) => {
+      const key = element;
+      key.innerHTML = KEYS[key.id][this.lang][this.layout];
+    });
   };
 }
